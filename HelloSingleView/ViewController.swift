be93e06192327d6b1bfc43d1e20648721b2e0984
ZyntaaX <rasmus.svahnberg97@gmail.com>
2019-10-23 12:23:15 +0200

    Initial Commit

---
+++ HelloSingleView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloSingleView
+//
+//  Created by Rasmus Svanberg on 2019-10-23.
+//  Copyright © 2019 Rasmus Svanberg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
