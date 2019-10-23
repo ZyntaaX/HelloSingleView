0171e7abd5c29cf052695fb42c570a84181235af
ZyntaaX <rasmus.svahnberg97@gmail.com>
2019-10-23 12:42:25 +0200

    Added Button

--- HelloSingleView/ViewController.swift
+++ HelloSingleView/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonClicked(_ sender: Any) {
+        print("Hello button")
+    }
 }
 
